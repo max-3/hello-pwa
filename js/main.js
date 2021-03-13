e40fad6bb19894964b5b1340ae257bdbbb643262
MAX3 <max3@gmail.comm>
2021-03-14 00:57:39 +0300

    back2

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./service-w.js');
+             .register('./service-w2.js');
   }
 }
--- service-w2.js
+++ service-w2.js
@@ -1,10 +1,10 @@
 var cacheName = 'hello-pwa';
-// var filesToCache = [
-  // './',
-  // './index.html',
+var filesToCache = [
+  './',
+  './index.html',
   // './css/style.css',
   // './js/main.js'
-// ];
+];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
