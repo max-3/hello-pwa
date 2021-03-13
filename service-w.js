9dc8de94d1e30002f4c4b426783abe73e924a447
MAX3 <max3@gmail.comm>
2021-03-14 00:50:47 +0300

    test2

--- service-w.js
+++ service-w.js
@@ -1,10 +1,10 @@
 var cacheName = 'hello-pwa';
-var filesToCache = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/main.js'
-];
+// var filesToCache = [
+  // './',
+  // './index.html',
+  // './css/style.css',
+  // './js/main.js'
+// ];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
